10205bf82d3a4f703d691e1f5bd7d408bb50c43e
Daria Davydenko <ya-ya-it@yandex.com>
2017-03-09 21:40:39 -0500

    Changed main page

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+
+<html lang="en-ca">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>
+        Mary Ventrella Associates
+    </title>
+    <!-- plugging in reset stylesheet, global stylesheet, and page-specific stylesheet-->
+    <link rel="stylesheet" type="text/css" href="css/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/pages/main.css"/>
+    <link rel="icon" href="images/icon.png"/>
+    <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./lib/font-awesome/css/font-awesome.css">
+
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<!-- NAVBAR -->
+
+<section class="poster">
+    <h1 class="col-lg-12">Invest in yourself and the relationship that are important to you!</h1>
+</section>
+
+<main class="main_block">
+    <article class="col-sm-12 col-md-12 col-lg-12">
+        <h3>WELCOME</h3>
+        <p>It is not easy to take that first step, but here you are, ready for change.
+            You are about to get the help you want to make the best choices for your life
+            by learning about yourself, and learning new ways to think and act. Psychotherapy
+            can not only help everyone see their own life more clearly and positively.
+            Psychotherapy provides the tools that increase self-knowledge, and develops skills to handle the
+            everyday
+            challenges that face you and everyone.</p>
+
+        <p>I have been privileged to work alongside hundreds of people who have wanted support and knowledge through
+            times of change or stress at home, at school or at work, or with their health or community.
+            Through guided self-discovery and mindfulness reflective practice, I would like to be there to help you
+            take
+            a more active part in building you a vibrant presence in and maximum enjoyment of your life.</p>
+
+        <p>By listening to what is most important to you, and then developing insights about your experiences, we
+            can
+            identify the causes and effects that hold you back, as well as discover the hidden passions, strengths
+            and
+            resources you will use to alter your course toward more joyful living. You have my promise
+            of compassion, open-mindedness and a creative attitude in every moment we share.</p>
+
+        <p>I very much look forward to working with you,
+            <br> Mary Ventrella</p>
+    </article>
+</main>
+<div class="row aside">
+    <div class="col-sm-7 col-md-7 col-lg-7">
+        <h1>Team building</h1>
+        <br/>
+        <article>
+            <h3>Brief info</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.</p>
+        </article>
+
+        <button type="button" class="btn btn-outline-success btn-lg col-lg-11 buttonLetsTalk">Let's talk!</a></button>
+    </div>
+    <div class="col-sm-5 col-md-5 col-lg-5">
+        <form method="post">
+            <fieldset>
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name" class="form-control" placeholder="Enter your name" required/>
+                <br/>
+                <label for="contact_info">Contact information:</label>
+                <input type="text" name="contact_info" id="contact_info" class="form-control"
+                       placeholder="Enter your contact info"
+                       required/>
+                <br/>
+                <label>Question:</label>
+                <br/>
+                <textarea class="form-control"></textarea>
+                <br/>
+                <button type="button" class="btn btn-success">Submit</button>
+            </fieldset>
+        </form>
+    </div>
+</div>
+
+<!-- Footer -->
+
+<!-- Plugging in the scripts for responsive navbar and alertbox -->
+<script src="js/privacy_policy.js"></script>
+<script src="js/dropdown.js"></script>
+<script src="./lib/jquery/dist/jquery.min.js"></script>
+<script src="./lib/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
